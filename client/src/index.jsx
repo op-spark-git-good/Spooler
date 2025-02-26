4d738813bfd473fd4ca9c5b1aba536a5aebc6a0c
khamal22 <yyeah3198@gmail.com>
2025-02-26 15:52:28 -0500

    ADD react router elements to index.jsx and App.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,8 +1,11 @@
 import React from "react";
+import App from "./App.jsx"
 import {createRoot} from "react-dom/client"
+import { BrowserRouter } from "react-router-dom";
 
 const appRoot = document.getElementById("app")
 
 const root = createRoot(appRoot)
 
-root.render(<p>hello world</p>)
+
+root.render(<BrowserRouter> <App /> </BrowserRouter>)
