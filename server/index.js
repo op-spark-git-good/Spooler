a56890137bcb5919a821493c78241544de4055a8
khamal22 <yyeah3198@gmail.com>
2025-02-26 18:14:11 -0500

    Remove cors

--- server/index.js
+++ server/index.js
@@ -1,5 +1,4 @@
 const express = require('express')
-const cors = require("cors")
 const path = require("path")
 const app = express()
 const port = 5500
@@ -7,7 +6,6 @@ const port = 5500
 
 //middleware
 app.use(express.json())
-app.use(cors())
 app.use(express.urlencoded({extended: true}))
 app.use(express.static(path.join(__dirname, "dist")))
 
