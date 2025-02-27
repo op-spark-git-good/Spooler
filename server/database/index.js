d25b1204691ff0e8bb796285a8f0d2a57e19063a
stephen <jimisax@gmail.com>
2025-02-27 14:21:36 -0600

    ADD: Functional Auth routing from upstream

--- server/database/index.js
+++ server/database/index.js
@@ -1,5 +1,5 @@
 const {connect, Schema} = require("mongoose");
-const { Fabrics } = require("server/database/models/Fabrics.js");
+const { Fabrics } = require("./models/Fabrics.js");
 
 const db = 'Spooler'
 const dbUri = `mongodb://localhost:27017/${db}`
