58ac87b940ee3c15e94ee428d899351b9f07dc5c
khamal22 <yyeah3198@gmail.com>
2025-02-27 12:18:54 -0500

    ADD Google authentication prototype complete

--- server/index.js
+++ server/index.js
@@ -1,22 +1,28 @@
-const express = require('express')
-const path = require("path")
-const app = express()
-const port = 5500
+const express = require("express");
+const passport = require('passport')
+const path = require("path");
+const app = express();
+const port = 5500;
 
+ require("./passport.js");
 
 //middleware
-app.use(express.json())
-app.use(express.urlencoded({extended: true}))
-app.use(express.static(path.join(__dirname, "dist")))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "dist")));
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.get("/", (req, res) => {
+  res.send('<a href="/auth/google"> Authenticate with Google </a>');
+});
 
+app.get("/auth/google",
+  passport.authenticate('google', {scope: ['email', 'profile']})
+)
 
-
-
+app.get("/protected", (req, res) => {
+  res.send("Hello world");
+});
 
 app.listen(port, () => {
-  console.log(`Spooler listening At http://localhost:${port}`)
-})
+  console.log(`Spooler listening At http://localhost:${port}`);
+});
