8c57a9aad3a649bcd96c6a8ea6d975c14680a5f2
stephen <jimisax@gmail.com>
2025-02-27 14:22:24 -0600

    REPLACE: hard code route to /fabrics to Fabrics component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ function App(){
   return (
     <Routes>
       {/*<Route path="/" element={}/>*/}
-      <Route path="/fabrics"  element={<div>What up, planet</div>} />
+      <Route path="/fabrics"  element={<Fabrics />} />
     </Routes>
   )
 }
