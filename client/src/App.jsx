d13b81a8dfe946e5d9bd1ca2f4e346eb130286ac
stephen <jimisax@gmail.com>
2025-02-27 15:51:22 -0600

    ADD: Home component for the root page. Routing to it in  App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Fabrics from "./Fabrics.jsx";
+import Home from "./Home.jsx";
 
 function App() {
   return (
     <Routes>
-      {/*<Route path="/" element={}/>*/}
+      <Route path="/" element={<Home />} />
       <Route path="/fabrics" element={<Fabrics />} />
     </Routes>
   );
