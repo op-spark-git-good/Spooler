c62502ea42845f615d2ce96285fda6117cd1b9a5
khamal22 <yyeah3198@gmail.com>
2025-02-27 14:33:32 -0500

    ADD setup Google Authentication

--- server/index.js
+++ server/index.js
@@ -1,27 +1,60 @@
 const express = require("express");
 const passport = require('passport')
 const path = require("path");
-const app = express();
+const session = require('express-session')
+require("./passport.js");
+require('dotenv').config();
+
 const port = 5500;
 
- require("./passport.js");
+
+function isLoggedIn(req, res, next){
+  req.user ? next() : res.sendStatus(401)
+}
+
+
+const app = express();
+
+//session configuration
+app.use(session({secret: 'Scrumbags'}))
+
+
+
+//passport initialization
+app.use(passport.initialize())
+app.use(passport.session())
 
 //middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "dist")));
 
+//Routes
 app.get("/", (req, res) => {
   res.send('<a href="/auth/google"> Authenticate with Google </a>');
 });
 
-app.get("/auth/google",
+app.get("/auth/google", 
   passport.authenticate('google', {scope: ['email', 'profile']})
-)
+);
 
-app.get("/protected", (req, res) => {
+app.get("/auth/google/callback",
+  passport.authenticate('google',
+  {failureRedirect: '/auth/failure'}),
+  (req, res) => {
+    res.redirect('/protected');
+  }
+);
+
+app.get('/auth/failure', (req, res) => {
+  res.send('Authentication failure')
+})
+
+app.get("/protected", isLoggedIn, (req, res) => {
   res.send("Hello world");
 });
+
+
 
 app.listen(port, () => {
   console.log(`Spooler listening At http://localhost:${port}`);
--- server/passport.js
+++ server/passport.js
@@ -12,10 +12,9 @@ passport.use(new GoogleStrategy({
     clientID: GOOGLE_CLIENT_ID,
     clientSecret: GOOGLE_CLIENT_SECRET,
     callbackURL: "http://localhost:5500/auth/google/callback",
-    passReqToCallback: true,
   },
   function(accessToken, refreshToken, profile, done) {
-     return done(err, profile);
+     return done(null, profile);
     // User.findOrCreate({ googleId: profile.id }, function (err, user) {
     //   return done(err, profile);
     // });
