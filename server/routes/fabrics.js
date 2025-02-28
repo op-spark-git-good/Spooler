10373998d0738533455da6e83c8c57589b3dd931
khamal22 <yyeah3198@gmail.com>
2025-02-28 17:15:50 -0500

    DELETE: fabrics request is being handle in index.js

--- server/routes/fabrics.js
+++ server/routes/fabrics.js
@@ -1,17 +1,14 @@
 const express = require("express");
 const router = express.Router();
-// const { Fabrics } = require("./database");
 
-// const fabricsRouter = Router();
+// router.get("/", async (req, res) => {
+// try {
 
-router.get("/", async (req, res) => {
-try {
-
-  res.send("Proof of concept");
-} catch (err) {
-  console.error(err);
-  res.sendStatus(500);
-}
-});
+//   res.send("Proof of concept");
+// } catch (err) {
+//   console.error(err);
+//   res.sendStatus(500);
+// }
+// });
 
 module.exports = router;
