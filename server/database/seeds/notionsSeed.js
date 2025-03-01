d5d3035e59ccfaa2c64c80f5f62686e0ab47e245
mydogditto <dhgoldberg15@gmail.com>
2025-03-01 15:41:57 -0600

    Add notionsSeed data

---
+++ server/database/seeds/notionsSeed.js
@@ -0,0 +1,9 @@
+const notionsData = [
+  { upc: "999990271692", title: "Gutermann 3000C-5709 Natural Cotton Thread Solids, 3281-Yard, White", color: 'White', image: "https://images.barcodespider.com/upcimage/999990271692.jpg", brand: 'Gutterman' },
+
+  { upc: "999990250499", title: "Gutermann 25049 Natural Cotton Thread Solids 876 Yards-White", color: 'White', image: "https://images.barcodespider.com/upcimage/999990250499.jpg", brand: 'Gutterman' },
+
+  { upc: "787162831429", title: "Gutermann 731323/1 Jeans Extra Strong Polyester Sewing Thread 100m x 3 200m x 2", color: 'Denim', image: "https://images.barcodespider.com/upcimage/787162831429.jpg", brand: 'Gutterman' },
+
+]
+module.exports = notionsData;
