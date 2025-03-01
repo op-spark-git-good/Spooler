dba38a7c342c78e3727c1c02bad67f3c99faf34b
stephen <jimisax@gmail.com>
2025-03-01 10:38:26 -0600

    ADD: seed Data file for fabrics. Care and Notes properties to schema

--- server/database/models/Fabrics.js
+++ server/database/models/Fabrics.js
@@ -5,10 +5,12 @@ const FabricsSchema = {
   image: { type: String },
   description: { type: String },
   quantity: { type: Number },
-  color: { type: String },
-  weave: { type: String },
-  brand: { type: String },
-  origin: { type: String },
+  color: [{ type: String }],
+  weave: [{ type: String }],
+  brand: { type: String, default: "unknown" },
+  origin: { type: String, default: "unknown" },
+  care: { type: String, default: "unknown" },
+  notes: {type: String, required: false },
 };
 
 const Fabrics = model("Fabric", new mongoose.Schema(FabricsSchema));
