d10d6841a57ce1303800272aad6c0ea9d05209cf
joseph <josephpfurman@gmail.com>
2025-03-03 02:43:56 -0600

    ADD; projects.js to server/routes, Projects.jsx to client/src. UPDATE; App.jsx to have Projects route, server/index.js to have projects router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,8 @@ import { Routes, Route } from "react-router-dom";
 import Fabrics from "./Fabrics.jsx";
 import Home from "./Home.jsx";
 import Layout from "./Layout.jsx";
-import Notions from "./NotionsList.jsx";
+import Notions from "./Notions.jsx";
+import Projects from "./Projects.jsx";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route index element={<Home />} />
           <Route path="fabrics" element={<Fabrics />} />
           <Route path="notions" element={<Notions />} />
+          <Route path="projects" element={<Projects />} />
         </Route>
       </Routes>
     </div>
