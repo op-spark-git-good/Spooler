e7781751baa133776126ea1e43d23d959580dc97
stephen <jimisax@gmail.com>
2025-03-03 10:47:48 -0600

    ADD: Straggling merge item App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,11 @@
 import React from "react";
-<<<<<<< HEAD
-import { Routes, Route, Link } from "react-router-dom";
-import Fabrics from "./Fabrics.jsx";
-import Home from "./Home.jsx";
-import Layout from "./Layout.jsx";
-import Notions from "./Notions.jsx";
-=======
 import {Route, Routes } from "react-router-dom";
-import Navbar from './NavBar.jsx'; 
+import Navbar from './NavBar.jsx';
 import Fabrics from "./Fabrics.jsx";
 import Home from "./Home.jsx";
 import Layout from "./Layout.jsx";
 import Notions from "./Notions/Notions.jsx";
 import Patterns from "./Patterns/Pattern.jsx";
->>>>>>> 25e58f3789caec46e49814af55dadb3fd5acde30
 
 function App() {
   return (
