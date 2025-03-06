8c9874452f119cb8640a6598c9800f10f0ee3182
joseph <josephpfurman@gmail.com>
2025-03-06 11:14:20 -0600

    ADD: Seed data and for projects UPDATE: Projects.jsx is now a functional component. It is now added to Routes and NavBar

---
+++ server/database/seeds/projectSeed.js
@@ -0,0 +1,48 @@
+const projects = [
+{
+  name: 'Prom Dress',
+  description: 'Daria wants an elegant navy dress',
+  tasks: [
+    {
+      name:'Cut fabric to pattern',
+      description: null,
+      isComplete: false,
+      priority: 1
+    },
+    {
+      name:'Ensure that the pattern is sized correctly',
+      description: "Meet with Daria on Friday to get measurements",
+      isComplete: false,
+      priority: 2      
+    }
+  ],
+  patterns: [
+    {
+    name: 'Fancy Dress',
+    description: 'A very fancy dress pattern',
+    default: null
+    }
+  ],
+  fabrics:[
+    {
+      description: 'Navy Satin',
+      quantity: 18,
+      stashed: null
+    }
+  ],
+  notions:[
+    {
+      description: 'needles',
+      quantity: 1000,
+      stashed: null
+    },
+    {
+      description: 'Navy thread',
+      quantity: 12000,
+      stashed: null
+    }
+  ],
+}
+]
+
+module.exports = projects
