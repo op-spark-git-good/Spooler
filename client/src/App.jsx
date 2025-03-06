468512553a72ed2025c0ec7627ffa56808084873
khamal22 <yyeah3198@gmail.com>
2025-03-05 20:17:09 -0500

    ADD: Imported new pattern components and Updated routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,28 +1,29 @@
-import React from "react";
-import {Route, Routes } from "react-router-dom";
-import Navbar from './NavBar.jsx'; 
-import Fabrics from "./Fabrics.jsx";
-import Home from "./Home.jsx";
-import Layout from "./Layout.jsx";
-import Notions from "./Notions/Notions.jsx";
-import Patterns from "./Patterns/Pattern.jsx";
-import PatternList from './Patterns/PatternList.jsx'
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Navbar from './NavBar.jsx';
+import Fabrics from './Fabrics.jsx';
+import Home from './Home.jsx';
+import Layout from './Layout.jsx';
+import Notions from './Notions/Notions.jsx';
+import Patterns from './Patterns/Pattern.jsx';
+import PatternList from './Patterns/PatternList.jsx';
 import PatternDetail from './Patterns/PatternDetail.jsx';
-import Posts from "./Posts/Posts.jsx";
-
+import Posts from './Posts/Posts.jsx';
+import PatternForm from './Patterns/PatternForm.jsx';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
       <Navbar /> {/* Include the Navbar */}
       <Routes>
-        <Route path="/" element={<Layout />} />
+        <Route path='/' element={<Layout />} />
         <Route index element={<Home />} />
-        <Route path="fabrics" element={<Fabrics />} />
-        <Route path="notions" element={<Notions />} />
-        <Route path="/patterns" element={<PatternList />} /> 
-        <Route path="/patterns/:id" element={<PatternDetail />} />
-        <Route path="posts" element={<Posts />} />
+        <Route path='fabrics' element={<Fabrics />} />
+        <Route path='notions' element={<Notions />} />
+        <Route path='/patterns' element={<PatternList />} />
+        <Route path='/create-pattern' element={<PatternForm />} />
+        <Route path='/patterns/:id' element={<PatternDetail />} />
+        <Route path='posts' element={<Posts />} />
       </Routes>
     </div>
   );
