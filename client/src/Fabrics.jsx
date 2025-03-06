9b4eeca9bfc4b2d9e467504bd80fc3b20cc8d68f
stephen <jimisax@gmail.com>
2025-03-05 19:04:24 -0600

    ADD: turnStyle function for iterating through fabrics Array

--- client/src/Fabrics.jsx
+++ client/src/Fabrics.jsx
@@ -4,53 +4,67 @@ import axios from 'axios';
 // This component will hold the list of fabrics
 
 const Fabrics = () => {
-  const [ fabrics, setFabrics ] = useState([]);
-  // set a state variable to act as the current front-facing fabric on the page
-  const [ currFabric, setCurrFabric ] = useState(fabrics[0]);
-  // also set a state number to index the current fabric.
-  const [fabricNum, setFabricNum] = useState(0);
+  const [fabrics, setFabrics] = useState([]);
+  const [fabricsNum, setFabricsNum] = useState(0);
+  const [currFabric, setCurrFabric] = useState(fabrics[0]);
   // have the below get request run upon mounting(with use effect);
   // create a request that takes in all fabric documents from the database
   const getAllFabrics = () => {
-    
+
     axios.get("/api/fabrics").then(({ data }) => {
       setFabrics(data);
+      setCurrFabric(data[0]);
     }).catch((err) => console.error(err));
   }
   useEffect(getAllFabrics, []);
 // maybe set the post function in here and pass it down as props so we can reset every time it gets called?
-  fabrics.forEach((fabric) => {
-    for (let key in fabric) {
-      if (key === "name") {
-        console.log(fabric[key]);
-      }
-    }
-    // console.log('and another one')
-    // console.log(fabric);
-  })
 
-  // {console.log('Rendering', fabrics[0].name )}
+// make a function that changes the currFabric variable to the next or previous number in the fabrics array
+const turnStyle = (direction) => {
+  const max = fabrics.length - 1;
+  switch (direction) {
+    case "forward":
+      if (fabricsNum === max) {
+        setFabricsNum(0);
+        setCurrFabric(fabrics[0]);
+      } else {
+        setFabricsNum(fabricsNum + 1);
+        setCurrFabric(fabrics[fabricsNum + 1]);
+      }
+      break;
+      case "back":
+        if (fabricsNum === 0) {
+          setFabricsNum(max);
+          setCurrFabric(fabrics[max]);
+        } else {
+          setFabricsNum(fabricsNum - 1);
+          setCurrFabric(fabrics[fabricsNum - 1]);
+        }
+        break
+  }
+
+}
+
   if (fabrics.length) {
-    fabrics.map((fabric) => {
       return (
-        <div key={fabric._id} className="fabric-block">
+        <div key={currFabric._id} className="fabric-block">
         <img
           className="fabric-image"
-          src={fabric.image}
+          src={currFabric.image}
+          style={{width: "300px"}}
           />
-        <div className="fabric-name">{fabric.name} {/*Ghost Sheet*/}</div>
-        <p className="fabric-description">{fabric.description}
-          {/*This fabric is great to give you a scare!! Nice, simple, comfy cotton
-          makes this a winner all through the summer. Or Halloween night!*/}
+        <div className="fabric-name">{currFabric.name}</div>
+        <p className="fabric-description">{currFabric.description}
         </p>
-        <div className="fabric-color">Color: {fabric.color.join(', ')} {/*white*/}</div>
-        <div className="fabric-quantity">Inventory: {fabric.quantity} {/*8 yards*/}</div>
-        <div className="fabric-weave">Type: {fabric.weave}{/*cotton*/}</div>
-        <div className="fabric-origin">From: {fabric.origin} {/*The Wayward Haberdashery*/}</div>
-        <div className="fabric-brand">Brand: {fabric.brand} {/*Boo-niversity*/}</div>
+        <div className="fabric-color">Color: {currFabric.color.join(', ')}</div>
+        <div className="fabric-quantity">Inventory: {currFabric.quantity}</div>
+        <div className="fabric-weave">Type: {currFabric.weave}</div>
+        <div className="fabric-origin">From: {currFabric.origin}</div>
+        <div className="fabric-brand">Brand: {currFabric.brand}</div>
+        <button className="fabric-changer-forward" onClick={() => turnStyle("forward")}>NEXT FABRIC</button>
+        <button className="fabric-changer-back" onClick={() => turnStyle("back")}>PREVIOUS FABRIC</button>
       </div>
         );
-      })
     } else {
       return (
         <button onClick={getAllFabrics}>GET FABRICS</button>
