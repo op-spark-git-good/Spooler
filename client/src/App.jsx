3d65a5463ef9ebfdee2206f1ba82377488fdaa9f
joseph <josephpfurman@gmail.com>
2025-03-08 19:01:31 -0600

    ADD: Projects directory, moved projects.jsx to it; ProjectForm.jsx UPDATE: App.jsx router points to new Projects directory

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -10,7 +10,7 @@ import PatternList from './Patterns/PatternList.jsx'
 import PatternDetail from './Patterns/PatternDetail.jsx';
 import Posts from './Posts/Posts.jsx';
 import PatternForm from './Patterns/PatternForm.jsx';
-import Projects from './Projects.jsx'
+import Projects from './Projects/Projects.jsx'
 
 function App() {
   return (
