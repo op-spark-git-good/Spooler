5d6f60f560fc198b593be1f3b789c1e3e943beb2
stephen <jimisax@gmail.com>
2025-03-09 22:19:05 -0500

    ADD: Workable form to FabricForm.jsx, additional prop functions added from Fabrics.jsx

--- client/src/FabricForm.jsx
+++ client/src/FabricForm.jsx
@@ -12,6 +12,7 @@ import { Typography,
   DialogContentText,
   DialogTitle,
   Button,
+  Container,
 } from "@mui/material";
 
 const FabricForm = ({getAllFabrics, currFabric}) => {
@@ -75,22 +76,60 @@ const abortEditMode = () => {
 }
 
 return (
-  <div>
-  
-     <button className="edit-button" onClick={editEntry}>EDIT THIS FABRIC</button>
+  <Container
+  maxWidth="md"
+  style={{
+    height: '100vh',
+    padding: '20px'
+  }}
+  >
+    <Button className="edit-button" onClick={editEntry}>EDIT THIS FABRIC</Button>
     <form>
-      <label htmlFor="name">Name:</label>
+   <TextField
+                  fullWidth
+                  label="Name"
+                  name="name"
+                  {...register("name", { required: "Name is required" })}
+                  required
+                  placeholder="Name fabric here"
+                  sx={{
+                    padding: "10px"
+                  }}
+                />
+      {/* <label htmlFor="name">Name:</label>
       <input {...register("name", {
         required: "Fabric must have a name"
       })}
       type="text"
       id="fab-name"
-      placeholder="Name fabric here"/><br></br>
-      <label htmlFor="image">Image URL:</label>
+      placeholder="Name fabric here"/><br></br> */}
+      <TextField
+                  fullWidth
+                  label="image"
+                  {...register("image")}
+                  placeholder="Place fabric URL"
+                  sx={{
+                    padding: "10px"
+                  }}
+                />
+      {/* <label htmlFor="image">Image URL:</label>
       <input {...register("image")}
       type="text" id="fab-image"
-      placeholder="Add picture URL" /><br></br>
-      <label htmlFor="description">Description:</label>
+      placeholder="Add picture URL" /><br></br> */}
+      <TextField
+                  fullWidth
+                  label="Description"
+                  name="description"
+                  type="textarea"
+                  {...register("description")}
+                  placeholder="Tell us about your fabric"
+                  multiline
+                  rows={4}
+                  sx={{
+                    padding: "10px"
+                  }}
+                />
+      {/* <label htmlFor="description">Description:</label>
       <input {...register("description")}
       type="textarea"
       id="fab-description"
@@ -102,47 +141,125 @@ return (
         width: 100,
         height: 100,
       }}
-      /><br></br>
-      <label htmlFor="quantity">Quantity:</label>
+      />
+      */}
+      <br></br>
+      <TextField
+                  fullWidth
+                  label="Quantity"
+                  name="quantity"
+                  type="number"
+                  {...register("quantity")}
+                  placeholder="How much ya got?"
+                  sx={{
+                    padding: "10px"
+                  }}
+                />
+      {/* <label htmlFor="quantity">Quantity:</label>
       <input {...register("quantity")}
       type="number"
-      id="fab-quantity-yds" /><br></br>
-      <label htmlFor="color">Color(s):</label>
+      id="fab-quantity-yds" /><br></br> */}
+      <TextField
+                  fullWidth
+                  label="Color"
+                  name="color"
+                  inputRef={register("colors")}
+                  placeholder="colors"
+                  sx={{
+                    padding: "10px"
+                  }}
+                />
+      {/* <label htmlFor="color">Color(s):</label>
       <input {...register("colors")}
       type="text"
       id="fab-color"
       placeholder="Which colors are most prominent?"
-      /><br></br>
-      <label htmlFor="weave">Fabric Type:</label>
+      /><br></br> */}
+      <TextField
+                  fullWidth
+                  label="Weave"
+                  name="weave"
+                  inputRef={register("weave")}
+                  placeholder="What is it made of?"
+                  sx={{
+                    padding: "10px"
+                  }}
+                />
+      {/* <label htmlFor="weave">Fabric Type:</label>
       <input {...register("weave")}
       type="text"
       id="fab-type"
       placeholder="What kind of fabric is this?"
-      /><br></br>
-      <label htmlFor="brand">Brand:</label>
+      /><br></br> */}
+      <TextField
+                  fullWidth
+                  label="Brand"
+                  name="brand"
+                  inputRef={register("brand")}
+                  placeholder="Who made it?"
+                  sx={{
+                    padding: "10px"
+                  }}
+                />
+      {/* <label htmlFor="brand">Brand:</label>
       <input {...register("brand")}
       type="text"
       id="fab-brand"
       placeholder="Leave blank if unknown"
-      /><br></br>
-      <label htmlFor="origin">Place of Origin:</label>
+      /><br></br> */}
+      <TextField
+                  fullWidth
+                  label="Origin"
+                  name="origin"
+                  inputRef={register("origin")}
+                  placeholder="Where'd you get it?"
+                  sx={{
+                    padding: "10px"
+                  }}
+                />
+      {/* <label htmlFor="origin">Place of Origin:</label>
       <input {...register("origin")}
       type="text"
       id="fab-origin"
       placeholder="Leave blank if unknown"
-      /><br></br>
-      <label htmlFor="care">Care Instructions:</label>
+      /><br></br> */}
+      <TextField
+                  fullWidth
+                  label="Care"
+                  name="care"
+                  inputRef={register("care")}
+                  placeholder="Instructions for care"
+                  multiline
+                  rows={2}
+                  sx={{
+                    padding: "10px"
+                  }}
+                />
+      {/* <label htmlFor="care">Care Instructions:</label>
       <input {...register("care")}
       type="textarea"
       id="fab-care"
       placeholder="Care requirements: Leave blank if unknown"
-      /><br></br>
-      <label htmlFor="notes">Additional Notes:</label>
+      /><br></br> */}
+      <TextField
+                  fullWidth
+                  label="Notes"
+                  name="notes"
+                  type="textarea"
+                  inputRef={register("notes")}
+                  placeholder="Anything else?"
+                  multiline
+                  rows={4}
+                  sx={{
+                    padding: "10px"
+                  }}
+                />
+      {/* <label htmlFor="notes">Additional Notes:</label>
       <input {...register("notes")}
       type="textarea"
       id="fab-notes"
       placeholder="Anything else you'd like to say?"
-      /><br></br>
+      /><br></br> */}
       {editMode
       ?
       // <input onClick={handleSubmit(putForm)} type="submit" value="submit"/>
@@ -155,7 +272,7 @@ return (
       <button onClick={handleSubmit(postForm)} type="submit" id="post-butt">SUBMIT FABRIC</button>
     }
     </form>
-    </div>
+    </Container>
   )
 }
 
--- client/src/Fabrics.jsx
+++ client/src/Fabrics.jsx
@@ -182,7 +182,6 @@ const deleteFabric = () => {
             currFabric={currFabric}
             getAllFabrics={getAllFabrics}
             handleClose={handleClose}
-            editMode={true}
             />
              </Dialog>
 
